a2369db392ff17ab7b1fcaca882c2a3ea55e3a16
GeoKauko <geokauko(at)gmail.com>
2026-06-08 15:32:45 +0200

    Update

---
+++ scripts/config_template.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Set your local root directory here
+ROOT_DIR = Path("/path/to/your/project")
+
+DATA_DIR = ROOT_DIR / "data"
+OUT_DIR  = ROOT_DIR / "data" / "output"
+OBIA_DIR = ROOT_DIR / "data" / "output" / "lake_outlines"
